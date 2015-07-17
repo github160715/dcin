6eaad8202add7b43ffcc90bd1f7e278db24d08e9
natasha <strelka1302@gmail.com>
2015-07-17 16:52:25 +0300

    latest

--- node/u3/handlers/vals.js
+++ node/u3/handlers/vals.js
@@ -2,8 +2,10 @@ var http = require('http');
 var request = require('request');
 var hostname = "influxdb";
 var url1 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM cpu_value LIMIT 1";
-var url2 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM memory_value LIMIT 1";
+var url2 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='free' LIMIT 1";
+var url3 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='used' LIMIT 1";
 var xxx;
+var dict={};
 function parse(res){
     var key = res.substring(res.indexOf('name')+7, res.indexOf('columns')-3);
     var i = res.indexOf('[['), j = res.indexOf(']]');
@@ -12,12 +14,13 @@ function parse(res){
     x[key] = vals.substring(vals.indexOf(',')+1, vals.length);
     return x;
 }
+
 function logger(er, re, body) {
-    if (er){
-        console.log(er);
-    }else {
-        console.log(body);
-        xxx.status(200).json(parse(body));
+    if (er){console.log(er);}
+    else {
+        dict['used'] = parse(body);
+        console.log(dict);
+        xxx.status(200).json(dict);
     }
 }
 function cpu(req, res){
@@ -25,7 +28,13 @@ function cpu(req, res){
     xxx = res;
 }
 function memory(req, res){
-    request.get(url2, logger);
+    request.get(url2, function (er, re, body){
+        if (er){console.log(er);}
+        else {
+            dict['free'] = parse(body);
+            request.get(url3, logger);
+        }
+    });
     xxx = res;
 }
 
