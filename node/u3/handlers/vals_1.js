557bf40c8028160a2f31ff116e55623349523a81
natasha <strelka1302@gmail.com>
2015-07-20 16:03:40 +0300

    fixed query

--- node/u3/handlers/vals_1.js
+++ node/u3/handlers/vals_1.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var request = require('request');
 var hostname = "influxdb_1";
-var url1 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM cpu_value LIMIT 1";
-var url2 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='free' LIMIT 1";
-var url3 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='used' LIMIT 1";
+var url1 = "http://"+hostname+":8086/query?db=collectd&q=SELECT value FROM cpu_value WHERE time > now() -"+p+"s LIMIT 1";
+var url2 = "http://"+hostname+
+    ":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='free' AND time > now() -"+p+"s LIMIT 1";
+var url3 = "http://"+hostname+
+    ":8086/query?db=collectd&q=SELECT value FROM memory_value WHERE type_instance='used' AND time > now() -"+p+"s LIMIT 1";
 var xxx;
 //var dict={};
 function parse(res){
