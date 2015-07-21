15b4d66d6ad8d2c0e4da4261b72ed70833fd7269
Mikhail <makarov.mikhail11@gmail.com>
2015-07-21 11:44:55 +0300

    Исправлена функция agents

--- server/server.py
+++ server/server.py
@@ -31,6 +31,7 @@ def execution(agents, period):
             sleep(period)
 
 if __name__ == "__main__":
+
     confs = get_conf()
     period = confs["period"]
     agents = create_agents(confs)
--- service_prototype/handlers/agents.js
+++ service_prototype/handlers/agents.js
@@ -1,29 +1,31 @@
+var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
-var mongoUrl = "mongodb://localhost:27017/TestDB";
-var Acollection = "Agents", Icollection = "Info";
+var mongoUrl =  'mongodb://localhost:27017/test';
+var Acollection = "agents", Icollection = "states";
 
-function errf(req, err){
-    if (err != null) {
-        req.status(500).json({"error" : err.message});
-        return;
-    }
-}
+
+var findAgents = function(db, callback) {
+    var cursor =db.collection('agents').find( ).toArray(function (err, docs) {
+        data = [];
+        assert.equal(null, err);
+        for (var i = 0; i < docs.length; i++) {
+            data.push(docs[i].name);
+            console.log(docs[i].name);
+        }
+        callback(data);
+    });
+};
+
+
 function agents(req, res) {
     MongoClient.connect(mongoUrl, function (err, db) {
-            errf(req, err);
-            var collection = db.collection(Acollection);
-            collection.find({name : {search : ""}}).toArray(
-                function (err, docs) {
-                    if (err != null) {
-                        req.status(500).json({"error" : err.message});
-                        return;
-                    }
-                    var a = {}, l = docs.length;
-                    for (var i = 0; i < l; i++) {a[i] = docs[i].name;}
-                    res.status(200).json(a);
-                }
-            );
-
+            if (err != null) {
+                res.status(200).json("error");
+            }
+            findAgents(db, function(data) {
+                db.close();
+                res.status(200).json(data);
+            });
         }
     );
 }
