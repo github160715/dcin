f7a3633c53245a1b864a5778dfa1915ba020b817
Mikhail <makarov.mikhail11@gmail.com>
2015-07-21 12:08:35 +0300

    Исправлена функция status

--- service_prototype/handlers/agents.js
+++ service_prototype/handlers/agents.js
@@ -4,9 +4,9 @@ var mongoUrl =  'mongodb://localhost:27017/test';
 var Acollection = "agents", Icollection = "states";
 
 
-var findAgents = function(db, callback) {
+var find_agents = function(db, callback) {
     var cursor =db.collection('agents').find( ).toArray(function (err, docs) {
-        data = [];
+        var data = [];
         assert.equal(null, err);
         for (var i = 0; i < docs.length; i++) {
             data.push(docs[i].name);
@@ -22,29 +22,35 @@ function agents(req, res) {
             if (err != null) {
                 res.status(200).json("error");
             }
-            findAgents(db, function(data) {
+            find_agents(db, function(data) {
                 db.close();
                 res.status(200).json(data);
             });
         }
     );
 }
+
+function find_status(db, callback) {
+    var cursor =db.collection('agents').find().toArray(function (err, docs) {
+        var data = {};
+        assert.equal(null, err);
+        for (var i = 0; i < docs.length; i++) {
+            data[docs[i].name] = docs[i].status;
+        }
+        callback(data);
+    });
+}
 function status(req, res){
-    MongoClient.connect(mongoUrl, function (err, db){
-        errf(req, err);
-        var collection = db.collection(collectionName);
-        collection.find({name : {search : ""}}).toArray(
-            function (err, docs) {
-                if (err != null) {
-                    req.status(500).json({"error" : err.message});
-                    return;
-                }
-                var a = {}, l = docs.length;
-                for (var i = 0; i < l; i++) {a[docs[i].name] = docs[i].stat;}
-                res.status(200).json(a);
+    MongoClient.connect(mongoUrl, function (err, db) {
+            if (err != null) {
+                res.status(200).json("error");
             }
-        );
-    })
+            find_status(db, function(data) {
+                db.close();
+                res.status(200).json(data);
+            });
+        }
+    );
 }
 function last_info(req, res){
     MongoClient.connect(mongoUrl, function (err, db){
