7919ca728b93c1c3d02b704177404e60579dca31
github160715 <github160715@users.noreply.github.com>
2015-07-22 10:43:45 +0300

    update get_last

--- service_prototype/handlers/agents.js
+++ service_prototype/handlers/agents.js
@@ -3,14 +3,13 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoUrl =  'mongodb://localhost:27017/test';
 var Acollection = "agents", Icollection = "states";
 
-
 var find_agents = function(db, callback) {
-    var cursor =db.collection('agents').find( ).toArray(function (err, docs) {
+    db.collection('agents').find( ).toArray(function (err, docs) {
         var data = [];
         assert.equal(null, err);
         for (var i = 0; i < docs.length; i++) {
             data.push(docs[i].name);
-            console.log(docs[i].name);
+    //        console.log(docs[i].name);
         }
         callback(data);
     });
@@ -31,7 +30,7 @@ function agents(req, res) {
 }
 
 function find_status(db, callback) {
-    var cursor =db.collection('agents').find().toArray(function (err, docs) {
+    db.collection('agents').find().toArray(function (err, docs) {
         var data = {};
         assert.equal(null, err);
         for (var i = 0; i < docs.length; i++) {
@@ -52,20 +51,27 @@ function status(req, res){
         }
     );
 }
+
+function errf(req, err){
+    if (err != null) {
+        req.status(500).json({"error" : err.message});
+        return;
+    }
+}
+
 function get_last(db, callback) {
-    var data = {}, ttt = 0;
+    var data = [], ttt = 0;
     db.collection('agents').find({}).each(function (err, docs){
-        if (docs == null) {
-            console.log(data);
-            return;
-        }
+        if (docs == null) return;
+        assert.equal(null, err);
         ttt++;
         var r = db.collection('states').find({time : docs.last}).toArray(function (er, ds){
-            data[docs.name] = ds;
-            if (--ttt == 0) callback(data);
+            data.push(ds[0]);
+            if (!(--ttt)) callback(data);
         });
     });
 }
+//теперь возвращает массив последних states (словарь был избыточным)
 function last_info(req, res){
     MongoClient.connect(mongoUrl, function (err, db){
         errf(res, err);
@@ -75,15 +81,14 @@ function last_info(req, res){
         });
     });
 }
-//localhost:3000/info/inf_date/sup_date
 function info(req, res){
     var a = req.params.inf, b = req.params.sup;
     MongoClient.connect(mongoUrl, function (err, db){
         errf(req, err);
-        var collection = db.collection(Icollection);
-        collection.find({time : {$gt: new Date(a), $lt: new Date(b)}}).toArray(
-            function (err, docs) {
-                errf(res, err);
+        db.collection(Icollection).find({time : {$gt: new Date(a), $lt: new Date(b)}}).toArray(
+            function (er, docs) {
+                errf(res, er);
+                db.close();
                 res.status(200).json(docs);
             }
         );
