36daf6e0cc9c80ae4131dae5ffa25d63b941d8c4
natasha <strelka1302@gmail.com>
2015-07-28 13:42:35 +0300

    POST

--- service_prototype/app.js
+++ service_prototype/app.js
@@ -4,22 +4,39 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var handlers = require('./routes/handlers');
 
 var app = express();
+var mongoUrl = 'mongodb://localhost:27017/test';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,11 +44,23 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/handlers', handlers);
 
+var queries = require('./handlers/queries');
+
+app.post('/add', function (req, res) {
+    queries.add(req.body, res);
+});
+app.post('/upd', function (req, res) {
+    queries.upd(req.body, res);
+});
+app.post('/del', function (req, res) {
+    queries.del(req.body, res);
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -39,23 +68,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
---
+++ service_prototype/handlers/queries.js
@@ -0,0 +1,81 @@
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/test';
+
+var check = function(db, col_names, field_names, res) {
+    for (var i in col_names) {
+        db.collection(col_names[i]).find({$or: field_names}).toArray(
+            function (er1, docs) {
+                if ((er1 || docs.length > 0) && !res._headerSent) {
+                    res.status(400).send("Something went wrong");
+                }
+            });
+    }
+};
+var add = function (doc, res) {
+    //status false
+    console.log('adding: ', doc);
+    MongoClient.connect(mongoUrl, function (er1, db) {
+        if (er1) {
+            res.status(301).send('MongoClient connection error');
+            return
+        }
+        check(db, ['agents'], [{"name": doc['name']}, {"http": doc['http']}], res);
+        db.collection('add').insertOne(doc, function (er3) {
+            if (er3 && !res._headerSent) {
+                res.status(302).send('Mongo collection insert error')
+            } else {
+                if (!res._headerSent) {
+                    console.log('add');
+                    res.status(200).send('added');
+                }
+            }
+        });
+    });
+};
+
+var upd = function (params, res) {
+  console.log('updating doc with id: ', params['id']);
+    MongoClient.connect(mongoUrl, function (er1, db) {
+        if (er1) {
+            res.status(301).send('MongoClient connection error');
+            return
+        }
+        check(db, ['agents', 'add'], [{"name": params['name']}, {"http": params['http']}], res);
+        db.collection('upd').findOneAndUpdate(
+            {"id": params['id']},
+            {$set: {
+                "name": params['name'],
+                "http": params['http'],
+                "period": params['period']}},
+            {upsert: true},
+            function(er2){
+                if (res._headerSent) {return}
+                if (er2) {res.status(302).send("Something went wrong"); return }
+                res.status(200).send('updated')
+            });
+    });
+};
+
+var del = function(obj, res){
+    console.log('deleting doc with id: ', obj['id']);
+    MongoClient.connect(mongoUrl, function (er1, db) {
+        if (er1) {
+            res.status(301).send('MongoClient connection error');
+            return
+        }
+        db.collection('del').updateOne(
+            {'id' : obj['id']},
+          //  {$set: {}},
+            {upsert: true},
+            function(er2){
+                console.log('reached');
+                if (er2) {res.status(302).send("Something went wrong"); return }
+                res.status(200).send('deleted')
+            }
+        );
+    });
+};
+
+module.exports.add = add;
+module.exports.upd = upd;
+module.exports.del = del;
