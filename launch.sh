3eae0e243a7a87e6a75f470f55732666708a301f
Mikhail <makarov.mikhail11@gmail.com>
2015-07-29 13:21:23 +0300

    Добавлено отображение папки csv контейнера на компьютер

--- launch.sh
+++ launch.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 for ((x=1; x<3; x++)) do
 	sudo docker run --name influxdb_${x} -d usr/influxdb
 	sudo docker run -v $(pwd)/collectd/collectd_${x}.conf:/etc/collectd/collectd.conf \
+	-v /home/$USER/csv_${x}/:/var/lib/collectd/csv/ \
 	--name collectd_${x} --link influxdb_${x} -d usr/collectd
 	echo $((x+3000))
 	sudo docker run -v $(pwd)/node/u3/handlers/vals_${x}.js:/src/handlers/vals.js \
